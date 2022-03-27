762a8d83418558680319771c010b77017eded87e
MK Pryor <97571602+mkp1200@users.noreply.github.com>
2022-03-27 02:57:23 -0500

    First commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,116 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  title = "Grocery";
+
+  items = [
+
+    {
+
+      name: "Milk",
+      quantity: 2
+    },
+    {
+
+      name: "Bread",
+      quantity: 4
+    },
+    {
+
+      name: "Rice",
+      quantity: 3
+    },
+
+
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {}
+
+  
+
+  removeItem(item, index) {
+    console.log("Removing Items - ", item, index);
+    this.toastCtrl.create({
+
+      message: "Removing Item  - " + index,
+      duration: 3000
+    }).then(res => res.present());
+    this.items.splice(index, 1);
+
+    
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showPrompt();
+   
+  }
+
+  showPrompt() {
+    const prompt = this.alertCtrl.create({ 
+
+      
+      message: "Please enter item...",
+      inputs: [
+         {
+
+           name: 'name',
+           placeholder: 'Name'
+         },
+         {
+           name: 'quantity',
+           placeholder: 'Quantity'
+         }  
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: item => {
+            console.log('Cancel clicked',);
+          }
+          
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+
+
+    }).then(res => res.present());
+    
+
+
+
+
+
+
+
+
+
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
